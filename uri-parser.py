5b198c53c53a277d9178e5e2496ae5f65184d198
kerolloz <kerolloz@yahoo.com>
2019-01-02 15:25:59 +0200

    final touch :100:

--- uri-parser.py
+++ uri-parser.py
@@ -1,4 +1,12 @@
-import
+import re
+import sys
+import urllib.request
+
+try:
+    from bs4 import BeautifulSoup
+except Exception as e:
+    raise e
+
 
 def problem_link_checker(url):
     link_format = 'https://www.urionlinejudge.com.br/repository/UOJ_{number}_en.html'
@@ -7,7 +15,49 @@ def problem_link_checker(url):
         return link_format.format(number=''.join(number))
     return None
 
-def main
+
+def get_tags_contents(html_souped, tag_name, class_name=None):
+    return [tag for tag in html_souped.find_all(tag_name, class_name)]
+
+
+def write_tags_to_files(tags_list, file_name="tag.txt"):
+    counter = 1
+    for tag in tags_list:
+        if tag.p is not None:
+            with open(str(counter) + file_name, 'w') as output_file:
+                for line in tag.p.contents:
+                    line = line.strip()  # clear all lines
+                    line += '\n'  # add new line to the right
+                    output_file.write(line.lstrip())
+                    # lstrip = strip the left part(if the line is empty it will remove the added new line)
+
+                counter += 1
+
+
+def main():
+    problem_link = sys.argv[1]
+    problem_link = problem_link_checker(problem_link)
+    # print(">> Please wait while downloading the submission content")
+    try:
+        my_request = urllib.request.urlopen(problem_link)
+        my_html = my_request.read()
+    except Exception as __connection_error__:
+        print(">> Something went wrong while reading the submission link!", __connection_error__)
+        return -1
+    html_souped = BeautifulSoup(my_html, 'lxml')
+
+    # replace <br> tags with new lines
+    # so writing tags to files work properly
+    for br_tag in html_souped.find_all("br"):
+        br_tag.replace_with('')
+
+    all_td_tags_list = get_tags_contents(html_souped, tag_name='td')
+    input_td_tags = get_tags_contents(html_souped, tag_name='td', class_name='division')
+    # trim white spaces in test cases
+    output_td_tags = list(set(all_td_tags_list) - set(input_td_tags))
+
+    write_tags_to_files(input_td_tags, '.in')
+    write_tags_to_files(output_td_tags, '.out')
 
 
 if __name__ == '__main__':
