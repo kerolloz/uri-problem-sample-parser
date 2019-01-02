69c6efe7f840c0ad01ded3d446bb284e0ac6e21a
kerolloz <kerolloz@yahoo.com>
2019-01-02 15:30:27 +0200

    creating a folder according to problem id

--- uri-parser.py
+++ uri-parser.py
@@ -1,6 +1,7 @@
 import re
 import sys
 import urllib.request
+import os
 
 try:
     from bs4 import BeautifulSoup
@@ -12,7 +13,7 @@ def problem_link_checker(url):
     link_format = 'https://www.urionlinejudge.com.br/repository/UOJ_{number}_en.html'
     number = re.findall("[0-9]\d+", url)
     if len(number) == 1:
-        return link_format.format(number=''.join(number))
+        return link_format.format(number=''.join(number)), number[0]
     return None
 
 
@@ -36,7 +37,7 @@ def write_tags_to_files(tags_list, file_name="tag.txt"):
 
 def main():
     problem_link = sys.argv[1]
-    problem_link = problem_link_checker(problem_link)
+    problem_link, number = problem_link_checker(problem_link)
     # print(">> Please wait while downloading the submission content")
     try:
         my_request = urllib.request.urlopen(problem_link)
@@ -51,6 +52,8 @@ def main():
     for br_tag in html_souped.find_all("br"):
         br_tag.replace_with('')
 
+    os.mkdir(number)
+    os.chdir(number)
     all_td_tags_list = get_tags_contents(html_souped, tag_name='td')
     input_td_tags = get_tags_contents(html_souped, tag_name='td', class_name='division')
     # trim white spaces in test cases
